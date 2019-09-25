efb253aaef3d45f738709b3ad0d26345d9938e70
Chris <christurner5@gmail.com>
2019-09-25 23:03:08 +0100

    Add initial code and some tracking of calls

--- src/main/java/personal/chris/leetcode/RegexMatching.java
+++ src/main/java/personal/chris/leetcode/RegexMatching.java
@@ -3,13 +3,48 @@ package personal.chris.leetcode;
 import java.util.regex.Pattern;
 
 /**
- * To solve
- * https://leetcode.com/problems/regular-expression-matching
+ * To solve <a href="https://leetcode.com/problems/regular-expression-matching">10. Regular Expression Matching</a>
  */
 public class RegexMatching {
 
-    public static boolean isMatch(String s, String p) {
-        return verify(s,p);
+    static int calls = 0;
+
+    public static boolean wrapper(String s, String p) {
+        calls = 0;
+
+        boolean result = isMatch(s, p);
+        System.out.println(calls);
+        return result;
+    }
+
+    private static boolean isMatch(String s, String p) {
+        calls++;
+
+        if (s.length() == 0 && p.length() == 0) {
+            return true; // We made it to the end of both strings!
+        }
+
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            // If we have a repeating character, try skipping it.
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            }
+        }
+        if (s.length() != 0 ^ p.length() != 0) {
+            // If only one string has characters left then we can't have a match
+            return false;
+        }
+        // Now we have both strings non-empty
+        if(s.charAt(0) != p.charAt(0) && p.charAt(0) != '.') {
+            // If the first characters of both strings aren't equal (up to wildcards) we've lost
+            return false;
+        }
+        if(p.length() > 1 && p.charAt(1) == '*') {
+            // We have a repeating char, so we have to branch to decide whether to remove the character or not
+            return isMatch(s.substring(1), p.substring(2)) || isMatch(s.substring(1), p);
+        } else {
+            return (isMatch(s.substring(1), p.substring(1)));
+        }
     }
 
     public static boolean verify(String s, String p) {
--- src/test/java/personal/chris/leetcode/RegexMatchingTest.java
+++ src/test/java/personal/chris/leetcode/RegexMatchingTest.java
@@ -9,92 +9,110 @@ class RegexMatchingTest {
 
     @Test
     void mismatch() {
-        boolean result = RegexMatching.isMatch("aa", "a");
+        boolean result = RegexMatching.wrapper("aa", "a");
         assertFalse(result);
     }
 
     @Test
     void mismatch2() {
-        boolean result = RegexMatching.isMatch("aa", "aaa");
+        boolean result = RegexMatching.wrapper("aa", "aaa");
         assertFalse(result);
     }
 
     @Test
     void Simplematch() {
-        boolean result = RegexMatching.isMatch("aa", "aa");
+        boolean result = RegexMatching.wrapper("aa", "aa");
         assertTrue(result);
     }
 
     @Test
     void Simplematch2() {
-        boolean result = RegexMatching.isMatch("abc", "abc");
+        boolean result = RegexMatching.wrapper("abc", "abc");
         assertTrue(result);
     }
 
     @Test
     void Repetition() {
-        boolean result = RegexMatching.isMatch("aba", "ab*a");
+        boolean result = RegexMatching.wrapper("aba", "ab*a");
         assertTrue(result);
     }
 
     @Test
     void Repetition2() {
-        boolean result = RegexMatching.isMatch("abbbb", "a*b*");
+        boolean result = RegexMatching.wrapper("abbbb", "a*b*");
+        assertTrue(result);
+    }
+
+    @Test
+    void Repetition3() {
+        boolean result = RegexMatching.wrapper("aabbbb", "c*a*b*c*");
         assertTrue(result);
     }
 
     @Test
     void RepetitionNegative() {
-        boolean result = RegexMatching.isMatch("abbbbc", "a*b*");
+        boolean result = RegexMatching.wrapper("abbbbc", "a*b*");
         assertFalse(result);
     }
 
     @Test
     void RepetitionNegative2() {
-        boolean result = RegexMatching.isMatch("ac", "a*b*");
+        boolean result = RegexMatching.wrapper("ac", "a*b*");
         assertFalse(result);
     }
 
     @Test
     void Wildcard() {
-        boolean result = RegexMatching.isMatch("ab", "a.");
+        boolean result = RegexMatching.wrapper("ab", "a.");
         assertTrue(result);
     }
 
     @Test
     void Wildcard2() {
-        boolean result = RegexMatching.isMatch("abc", "..c");
+        boolean result = RegexMatching.wrapper("abc", "..c");
         assertTrue(result);
     }
 
     @Test
     void WildcardNegative() {
-        boolean result = RegexMatching.isMatch("ab", "a.b");
+        boolean result = RegexMatching.wrapper("ab", "a.b");
         assertFalse(result);
     }
 
     @Test
     void WildcardNegative2() {
-        boolean result = RegexMatching.isMatch("aa", ".");
+        boolean result = RegexMatching.wrapper("aa", ".");
         assertFalse(result);
     }
 
     @Test
+    void WildcardRepetition() {
+        boolean result = RegexMatching.wrapper("bbbba", ".*a*a");
+        assertTrue(result);
+    }
+
+    @Test
     void EmptyPattern() {
-        boolean result = RegexMatching.isMatch("aa", "");
+        boolean result = RegexMatching.wrapper("aa", "");
         assertFalse(result);
     }
 
     @Test
     void EmptyString() {
-        boolean result = RegexMatching.isMatch("", ".");
+        boolean result = RegexMatching.wrapper("", ".");
         assertFalse(result);
     }
 
     @Test
     void EmptyBoth() {
-        boolean result = RegexMatching.isMatch("", "");
+        boolean result = RegexMatching.wrapper("", "");
         assertTrue(result);
+    }
+
+    @Test
+    void LongPattern() {
+        boolean result = RegexMatching.wrapper("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c");
+        assertFalse(result);
     }
 
 
