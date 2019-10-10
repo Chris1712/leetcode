122c6452131b3737f32dba962d2d291aca58264e
Chris <christurner5@gmail.com>
2019-10-10 22:48:47 +0100

    Add edge case logic and some more test cases for problem #30

--- src/main/java/personal/chris/leetcode/SubstringConcatenation.java
+++ src/main/java/personal/chris/leetcode/SubstringConcatenation.java
@@ -1,5 +1,6 @@
 package personal.chris.leetcode;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,17 +10,30 @@ import java.util.List;
 public class SubstringConcatenation {
 
     public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> indices = new ArrayList<>();
 
-        // Edge cases:
-        if (s == null || s.length() == 0) {
+        // Edge cases for words:
+        if (words == null || words.length == 0) {
             return Collections.emptyList();
         }
 
-        for (int i = 0; i < s.length(); i++) {
+        // Get total length of string to match:
+        int matchLength = 0;
+        for (int i = 0; i < words.length; i++) {
+            matchLength += words[i].length();
+        }
+
+        // Edge cases for string:
+        if (s == null || s.length() < matchLength) {
+            return Collections.emptyList();
+        }
+
+        for (int i = 0; i < (s.length()-matchLength); i++) {
+            String subString = s.substring(i, i+matchLength);
 
         }
 
-
+        return indices;
     }
 
 }
--- src/test/java/personal/chris/leetcode/SubstringConcatenationTest.java
+++ src/test/java/personal/chris/leetcode/SubstringConcatenationTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,9 +22,33 @@ class SubstringConcatenationTest {
     }
 
     @Test
-    void mismatch() {
+    void oneMatch() {
+        List<Integer> result = substr.findSubstring("barfodthefoobarman", new String[]{"foo", "bar"});
+        assertEquals(Arrays.asList(9), result);
+    }
+
+    @Test
+    void twoMatch() {
         List<Integer> result = substr.findSubstring("barfoothefoobarman", new String[]{"foo", "bar"});
-        assertEquals(Arrays.asList(0,9), result);
+        assertEquals(Arrays.asList(0, 9), result);
+    }
+
+    @Test
+    void noMatch() {
+        List<Integer> result = substr.findSubstring("barfoothefoobarman", new String[]{"foo", "bar", "hey"});
+        assertEquals(Collections.emptyList(), result);
+    }
+
+    @Test
+    void emptyString() {
+        List<Integer> result = substr.findSubstring("", new String[]{"foo", "bar"});
+        assertEquals(Collections.emptyList(), result);
+    }
+
+    @Test
+    void listTooLong() {
+        List<Integer> result = substr.findSubstring("foo", new String[]{"foo", "bar"});
+        assertEquals(Collections.emptyList(), result);
     }
 
 }
