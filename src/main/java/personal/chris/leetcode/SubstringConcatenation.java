667fd4db7cb15400cbda577efd8135c6e8a1571d
Chris <christurner5@gmail.com>
2019-11-06 16:10:31 +0000

    Add extra tests and solution for #30

--- src/main/java/personal/chris/leetcode/SubstringConcatenation.java
+++ src/main/java/personal/chris/leetcode/SubstringConcatenation.java
@@ -1,6 +1,7 @@
 package personal.chris.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,30 +11,41 @@ import java.util.List;
 public class SubstringConcatenation {
 
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> indices = new ArrayList<>();
-
         // Edge cases for words:
         if (words == null || words.length == 0) {
             return Collections.emptyList();
         }
 
-        // Get total length of string to match:
-        int matchLength = 0;
-        for (int i = 0; i < words.length; i++) {
-            matchLength += words[i].length();
-        }
+        List<Integer> indices = new ArrayList<>();
+        int wordLength = words[0].length(); // Length of each word in list
+        int candidateLength = wordLength * words.length; // Total length of string to match:
+
 
         // Edge cases for string:
-        if (s == null || s.length() < matchLength) {
+        if (s == null || s.length() < candidateLength) {
             return Collections.emptyList();
         }
 
-        for (int i = 0; i < (s.length()-matchLength); i++) {
-            String subString = s.substring(i, i+matchLength);
+        // Iterate through each string that could be a concatenation of the provided words
+        for (int i = 0; i <= (s.length()-candidateLength); i++) {
+            String candidate = s.substring(i, i+candidateLength);
 
+            List<String> tempList = new ArrayList(Arrays.asList(words));
+            // Iterate through the words in the string - remember each member of words is the same length (wordLength)
+            for (int j = 0; j < words.length; j++) {
+                Integer index = j * wordLength;
+                String word = candidate.substring(index, index + wordLength);
+                if (!tempList.contains(word)) {
+                    break;
+                }
+                tempList.remove(word);
+            }
+
+            if (tempList.isEmpty()) {
+                indices.add(i);
+            }
         }
 
         return indices;
     }
-
 }
