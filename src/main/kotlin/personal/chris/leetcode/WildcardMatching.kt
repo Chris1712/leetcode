a81708ccf2dae9c344a68feca93869866c963647
Chris <christurner5@gmail.com>
2022-09-24 14:51:53 +0100

    Kotlin fixes

---
+++ src/main/kotlin/personal/chris/leetcode/WildcardMatching.kt
@@ -0,0 +1,13 @@
+package personal.chris.leetcode
+
+/**
+ * To solve <a href="https://leetcode.com/problems/wildcard-matching/">44. Wildcard Matching</a>
+ * HARD
+ */
+class WildcardMatching {
+
+    fun isMatch(s: String, p: String): Boolean {
+        return false
+    }
+
+}
